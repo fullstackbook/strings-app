b0aa131b46afea0baf84c1b4e95d52137e34ad06
Travis Luong <travis@travisluong.com>
2023-08-05 13:28:24 -0700

    auth middleware

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const authenticatedAPIRoutes = [pathname.startsWith("/api/users")];
+
+  if (authenticatedAPIRoutes.includes(true)) {
+    const cookie = request.cookies.get("jwt-token");
+
+    if (!cookie || !cookie?.value) {
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
+    }
+
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+      await jwtVerify(cookie.value, secret);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        { error: "internal server error" },
+        { status: 500 }
+      );
+    }
+  }
+}
+
+export const config = {
+  matcher: "/:path*",
+};
