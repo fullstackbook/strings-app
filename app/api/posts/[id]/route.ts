e9d1c3ba1e4163f7eab014f97d9efd9f0bc40bd7
Travis Luong <travis@travisluong.com>
2023-08-06 15:35:35 -0700

    update post endpoint

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -16,3 +16,24 @@ export async function GET(
   }
   return NextResponse.json({ data: res.rows[0] });
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  const jwtPayload = await getJWTPayload();
+  const res = await sql("select * from posts where user_id = $1 and id = $2", [
+    jwtPayload.sub,
+    params.id,
+  ]);
+  if (res.rowCount == 0) {
+    return NextResponse.json({ error: "not found" }, { status: 404 });
+  }
+  await sql("update posts set content = $1 where user_id = $2 and id = $3", [
+    body.content,
+    jwtPayload.sub,
+    params.id,
+  ]);
+  return NextResponse.json({ msg: "update success" });
+}
