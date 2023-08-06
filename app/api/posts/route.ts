23c4df0a2393a1745b10e30799e97b0b53719630
Travis Luong <travis@travisluong.com>
2023-08-06 15:13:57 -0700

    get posts endpoint

---
+++ app/api/posts/route.ts
@@ -0,0 +1,26 @@
+import { getJWTPayload } from "@/app/util/auth";
+import { sql } from "@/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const jwtPayload = await getJWTPayload();
+  const { searchParams } = new URL(request.url);
+  const username = searchParams.get("username");
+  const page =
+    (searchParams.get("page") && parseInt(searchParams.get("page")!)) || 0;
+  const limit = 3;
+  const offset = page * 3;
+
+  const statement = `select p.*, u.avatar, u.username
+    from posts p inner join users u
+    on p.user_id = u.id where user_id = $1
+    order by created_at desc limit $2 offset $3`;
+
+  if (username) {
+    // TODO
+  }
+
+  const res = await sql(statement, [jwtPayload.sub, limit, offset]);
+
+  return NextResponse.json({ data: res.rows });
+}
