d564ac27d07cea020579e291ad6d9b0a1f5277ac
Travis Luong <travis@travisluong.com>
2023-08-12 14:18:44 -0700

    finalize search bar

--- app/(private)/search-bar.tsx
+++ app/(private)/search-bar.tsx
@@ -1,8 +1,26 @@
 import * as _ from "lodash";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import User from "../components/user";
 
 export default function SearchBar() {
   const [searchResults, setSearchResults] = useState([]);
+  const [visible, setVisible] = useState(true);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      // @ts-ignore
+      if (ref.current && !ref.current.contains(e.target)) {
+        setVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  });
 
   const debouncedFetchSearchResults = _.debounce(fetchSearchResults, 500);
 
@@ -11,8 +29,11 @@ export default function SearchBar() {
     if (res.ok) {
       const json = await res.json();
       console.log(json);
-
+      setVisible(true);
       setSearchResults(json.data);
+    } else {
+      setSearchResults([]);
+      setVisible(false);
     }
   }
 
@@ -21,14 +42,37 @@ export default function SearchBar() {
     debouncedFetchSearchResults(e.target.value);
   }
 
+  function handleClick(e: React.MouseEvent<HTMLInputElement>) {
+    setVisible(true);
+  }
+
   return (
-    <div>
+    <div
+      className="flex flex-row max-w-md w-full justify-end relative"
+      ref={ref}
+    >
       <input
         onChange={handleChange}
+        onClick={handleClick}
         type="text"
-        className="p-2 rounded-lg bg-gray-700 my-2"
+        className="p-2 rounded-lg bg-gray-700 my-2 max-w-xs"
         placeholder="Search"
       />
+      {visible && searchResults.length > 0 && (
+        <ul className="flex flex-col bg-gray-700 absolute p-2 rounded-lg top-14 w-full max-w-sm right-2">
+          {searchResults.map((res: UserI) => {
+            return (
+              <li
+                key={res.id}
+                className="my-3"
+                onClick={() => setVisible(false)}
+              >
+                <User user={res} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
