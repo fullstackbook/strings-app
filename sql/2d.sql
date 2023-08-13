09fbae255a0d08074a437a024fda5d6e5e3ec12c
Travis Luong <travis@travisluong.com>
2023-08-12 17:46:32 -0700

    is_misinformation migration

---
+++ sql/2d.sql
@@ -0,0 +1,2 @@
+alter table posts drop column is_misinformation;
+alter table posts drop column is_misinformation_flagged_at;
---
+++ sql/2u.sql
@@ -0,0 +1,2 @@
+alter table posts add column is_misinformation boolean;
+alter table posts add column is_misinformation_flagged_at timestamp;
